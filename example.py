585c9c1ab9a3380fe23ec699a6b35c659bdecf94
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2021-10-13 10:45:02 -0400

    change args to .add

--- example.py
+++ example.py
@@ -18,17 +18,15 @@ wsi_source = "foo.svs"
 # Add an entry with the inference result ...
 plugin_state.add(
     cell_type="tumor",
-    model_probability=0.84,
+    probability=0.84,
     polygon_coords=[(1, 1), (1, 0), (0, 0), (0, 1), (1, 1)],
-    wsi_source=wsi_source,
 )
 
 # ... multiple times...
 plugin_state.add(
     cell_type="lymphocyte",
-    model_probability=0.92,
+    probability=0.92,
     polygon_coords=[(10, 10), (10, 5), (5, 5), (5, 10), (10, 10)],
-    wsi_source=wsi_source,
 )
 
 # Save RDF graph to file.
