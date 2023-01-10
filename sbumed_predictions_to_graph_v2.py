28a69246ebfa76916155d901c27d4806607e6dbe
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2023-01-10 15:29:28 -0500

    add createAction and HalcyonROCrate

--- sbumed_predictions_to_graph_v2.py
+++ sbumed_predictions_to_graph_v2.py
@@ -8,6 +8,7 @@ import functools
 import gzip
 import hashlib
 from pathlib import Path
+from typing import List
 
 import openslide
 from rdflib import BNode
@@ -22,7 +23,15 @@ from shapely.geometry import box as _box
 dcmi = Namespace("http://purl.org/dc/terms/")
 exif = Namespace("http://www.w3.org/2003/12/exif/ns#")
 hal = Namespace("https://www.ebremer.com/halcyon/ns/")
+sdo = Namespace("https://schema.org/")
 w3_oa = Namespace("http://www.w3.org/ns/oa#")
+
+
+def _get_timestamp() -> str:
+    import datetime
+
+    dt = datetime.datetime.now()
+    return dt.strftime("%Y-%m-%d %H:%M:%S")
 
 
 def _md5sum(path) -> str:
@@ -43,7 +52,15 @@ class State:
     Add patch info to the state using `.add_patch()`. Write the output using `.write()`.
     """
 
-    def __init__(self, slide_path):
+    def __init__(
+        self,
+        slide_path,
+        name: str,
+        description: str,
+        github_url: str,
+        orcid_url: str,
+        keywords: List[str],
+    ):
         slide_path = Path(slide_path)
         if not slide_path.exists():
             raise FileNotFoundError(f"slide not found: {slide_path}")
@@ -51,10 +68,10 @@ class State:
         self._slide_md5 = _md5sum(slide_path)
         self._graph = Graph()
 
+        # Add slide dimensions.
         with openslide.open_slide(slide_path) as oslide:
             slide_width, slide_height = oslide.dimensions
         del oslide
-
         self._slideref = URIRef(f"urn:md5:{self._slide_md5}")
         self._graph.add(
             (self._slideref, exif.height, Literal(slide_height, datatype=XSD.int))
@@ -63,10 +80,51 @@ class State:
             (self._slideref, exif.width, Literal(slide_width, datatype=XSD.int))
         )
 
+        # Make the CreateAction node.
+        create_action = URIRef("https://bmi.stonybrookmedicine.edu/wsinfer/tcga/2023")
+        self._graph.add((create_action, RDF.type, sdo.CreateAction))
+        self._graph.add((create_action, sdo.name, Literal(name)))
+        self._graph.add((create_action, sdo.description, Literal(description)))
+        self._graph.add(
+            (
+                create_action,
+                sdo.instrument,
+                URIRef(github_url),
+            )
+        )
+        self._graph.add((create_action, sdo.object, self._slideref))
+        self._graph.add((create_action, sdo.result, URIRef("")))
+
+        # Make the HalcyonROCrate node.
+        rocrate_node = BNode()
+        self._graph.add((rocrate_node, RDF.value, hal.HalcyonROCrate))
+        self._graph.add((rocrate_node, sdo.creator, URIRef(orcid_url)))
+        self._graph.add((rocrate_node, sdo.datePublished, Literal(_get_timestamp())))
+        self._graph.add((rocrate_node, sdo.description, Literal(description)))
+        if keywords is not None:
+            for keyword in keywords:
+                self._graph.add((rocrate_node, sdo.keywords, Literal(keyword)))
+        self._graph.add(
+            (
+                rocrate_node,
+                sdo.license,
+                URIRef("https://creativecommons.org/licenses/by-nc-sa/3.0/au/"),
+            )
+        )
+        self._graph.add((rocrate_node, sdo.name, Literal(name)))
+        self._graph.add(
+            (rocrate_node, sdo.publisher, URIRef("https://ror.org/01882y777"))
+        )
+        self._graph.add(
+            (rocrate_node, sdo.publisher, URIRef("https://ror.org/05qghxh33"))
+        )
+        self._graph.add((rocrate_node, hal.hasCreateAction, create_action))
+
         self._graph.bind("hal", hal)
         self._graph.bind("oa", w3_oa)
         self._graph.bind("dcmi", dcmi)
         self._graph.bind("exif", exif)
+        self._graph.bind("so", sdo)
 
     def add_patch(self, *, minx, miny, maxx, maxy, probability: float, classname: str):
         geom = _box(minx=minx, miny=miny, maxx=maxx, maxy=maxy)
