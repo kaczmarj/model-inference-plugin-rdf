91cc76f46b949260c7f0d005376ffab1559ab5ca
Jakub Kaczmarzyk <jakub.kaczmarzyk@gmail.com>
2023-01-11 11:50:46 -0500

    write state within loop

--- sbumed_predictions_to_graph_v2.py
+++ sbumed_predictions_to_graph_v2.py
@@ -239,9 +239,8 @@ def main(
                     probability=row[f"prob_{col}"],
                     classname=col,
                 )
+            state.write(output_path)
             break
-
-    state.write(output_path)
 
 
 if __name__ == "__main__":
